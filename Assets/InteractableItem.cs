98a33243ec4d74ea653cafa22c602cea9ce05106
James <jlinnega@thoughtworks.com>
2016-10-21 09:33:33 +0100

    Kitten building blocks jigsaw with controllers and coliders

---
+++ Assets/InteractableItem.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableItem : MonoBehaviour {
+	public Rigidbody rigidbody;
+
+	private bool currentlyInteracting;
+	private WandController attatchedWand;
+
+	private Vector3 posDelta;
+	private Quaternion rotationDelta;
+	private float angle;
+	private Vector3 axis;
+	private float rotationFactor = 400f;
+	private float velocityFactor = 20000f;
+
+	private Transform interactionPoint;
+
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+		interactionPoint = new GameObject ().transform;
+		velocityFactor /= rigidbody.mass;
+		rotationFactor /= rigidbody.mass;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (attatchedWand != null && currentlyInteracting) {
+			posDelta = attatchedWand.transform.position - interactionPoint.position;
+			this.rigidbody.velocity = posDelta * velocityFactor * Time.fixedDeltaTime;
+
+			rotationDelta = attatchedWand.transform.rotation * Quaternion.Inverse (interactionPoint.rotation);
+			rotationDelta.ToAngleAxis (out angle, out axis);
+
+			if (angle > 180) {
+				angle -= 360;
+			}
+
+			this.rigidbody.angularVelocity = (Time.fixedDeltaTime * angle * axis) * rotationFactor ; 
+		}
+	}
+
+	public void BeginInteraction(WandController wand){
+		attatchedWand = wand;
+		interactionPoint.position = wand.transform.position;
+		interactionPoint.rotation = wand.transform.rotation;
+		interactionPoint.SetParent (transform, true);
+
+		currentlyInteracting = true;
+	}
+
+	public void EndInteraction(WandController wand){
+		if(wand == attatchedWand){
+			attatchedWand = null;
+			currentlyInteracting = false;
+
+		}
+	}
+
+	public bool IsInteracting(){
+		return currentlyInteracting;
+	}
+		
+}
---
+++ Assets/WandController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WandController : MonoBehaviour {
+
+	private SteamVR_TrackedObject trackedObj;
+	private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input ((int)trackedObj.index); } }
+
+	private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+	private GameObject pickUp;
+
+	HashSet<InteractableItem> objectsHoveringOver = new HashSet<InteractableItem>();
+
+	private InteractableItem closestItem;
+	private InteractableItem interactingItem;
+
+	// Use this for initialization
+	void Start () {
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	// Update is called once per frame 
+	void Update () {
+		if(controller == null) {
+			Debug.Log ("Controller not initialised");
+			return;
+		}
+
+
+		if (controller.GetPressDown (triggerButton)) {
+			Debug.Log ("Trigger button was just pressed");
+			float minDistance = float.MaxValue;
+			float distance;
+			foreach (InteractableItem item in objectsHoveringOver) {
+				distance = (item.transform.position - transform.position).sqrMagnitude; // squared because the difference in vectors can be a negative
+			
+
+				if (distance < minDistance) {
+					minDistance = distance;
+					closestItem = item;
+
+				}
+			}
+			interactingItem = closestItem;
+
+			if (interactingItem) {
+				//if (interactingItem.IsInteracting) {
+					interactingItem.EndInteraction (this);
+				//}
+				interactingItem.BeginInteraction (this);
+			}
+		}
+
+		if (controller.GetPressUp (triggerButton) && interactingItem != null) {
+			Debug.Log ("Trigger button was just Released");
+			interactingItem.EndInteraction (this);
+
+		}
+
+
+	}
+
+	private void OnTriggerEnter(Collider collider){
+		InteractableItem collidedItem = collider.GetComponent<InteractableItem> ();
+		if(collidedItem != null){ // is the item interactable?
+			objectsHoveringOver.Add(collidedItem);
+		}
+	}
+
+	private void OnTriggerExit(Collider collider){
+		InteractableItem collidedItem = collider.GetComponent<InteractableItem> ();
+		if(collidedItem != null){ // is the item interactable?
+			objectsHoveringOver.Remove(collidedItem);
+		}
+	}
+}
